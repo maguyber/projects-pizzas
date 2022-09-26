449aca6081a479820a9669a6bf0af254a7ac0f9e
Fabian <104913665+maguyber@users.noreply.github.com>
2022-09-26 20:23:12 -0300

    archivo de proyect_pizzas_front_end

--- welcome.php
+++ welcome.php
@@ -11,72 +11,21 @@
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
     />
     
+    <script src="js/jquery-3.6.1.min.js"></script>
+    <script src="js/bootbox.min.js"></script>
+    <script> function deleteProduct(cod){
+      bootbox.confirm("Desea usted Eliminar el producto "+cod, function(result){
+        if(result){
+          window.location="delete.php?q="+cod;
+        }
+      });
+    }
+    </script>
 
     <title>Document</title>
 </head>
 <body>
 <?php
-$table="<table class=table boardhead table striped'>
-foreach ($data as $row)
-{
-
-}
-<tr>
-<td>
-<?php
-echo $row["id_product"]
-?>
-</td>
-<td>
-<?php
-</td>
-echo $row["product name"]
-?>
-</td>
-<td>
-<?php
-</td>
-echo $row["product gallery"]
-?>
-</td>
-<td>
-<?php
-</td>
-echo $row["product price"]
-?>
-</td>
-<td>
-<?php
-</td>
-echo $row["fecha de alta"]
-?>
-</td>
-<td>
-<?php
-</td>
-echo $row["eliminar"]
-?>
-</td>
-<td>
-<?php
-</td>
-echo $row["actualizar"]
-?>
-</td>
-<td>
-<?php
-</td>
-</tr></head></body>
-echo$table;
-<a href="#"onclick="delete
-<a href="#"onclik="update product(<?php echo $row ['id product'] ?>)"> actualizar producto </a>
-</td>
-</tr>
-<?php 
-}
-echo "</tbody>";
-echo "/table">;
-
 
 session_start();
 if($_SESSION['logueado'])
@@ -86,6 +35,60 @@ echo "<br>";
 echo "Horario de conexion".$_SESSION['time'];
 echo "<br>";
 echo "<a href='logout.php'>logout</a>";
+echo "<a href='insert_products.php' class='btn btn-primary'> Ingresar Producto </a>";
+include_once("config_products.php");
+include_once("db.php");
+$link = new Db();
+$sql = "select p.id_product, p.product_name, p.price, date_format(p.start_date, '%d/%m/%Y') as date, c.category_name from products p inner join categories c on c.id_category=p.id_category order by c.category_name asc";
+$stmt = $link->prepare($sql);
+$stmt->execute();
+$data=$stmt->fetchAll();
+
+$table="<table class='table table-bordered table-striped'> <thead class='thead-dark'> <tr> <th> Id </th> <th> Producto </th> <th> Categoria </th> <th> Precio </th> <th> Fecha de Alta </th> <th> Eliminar </th> <th> Actualizar </th> </tr> </thead> <tbody>";
+echo $table;
+
+
+  foreach($data as $row){
+?>
+<tr>
+  <td>
+    <?php
+    echo $row['id_product'];
+    ?>
+    </td>
+    <td>
+    <?php
+    echo $row['product_name'];
+    ?>
+    </td>
+    <td>
+    <?php
+    echo $row['category_name'];
+    ?>
+    </td>
+    <td>
+    <?php
+    echo $row['price'];
+    ?>
+    </td>
+    <td>
+    <?php
+    echo $row['date'];
+    ?>
+    </td>
+    
+    <td>
+      <a href="#" onclick="deleteProduct(<?php echo $row['id_product'] ?>)"> Eliminar Producto </a>
+  </td>
+
+  <td>
+      <a href="#" onclick="updateProduct(<?php echo $row['id_product'] ?>)"> Actualizar Producto </a>
+  </td>
+  </tr>
+<?php
+}
+echo "</tbody>";
+echo "</table>";
 }
 else
 {header('location:login.html');}
