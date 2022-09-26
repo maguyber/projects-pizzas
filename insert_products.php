fc771224ffda7ff841cfac9b76317f690da78998
Fabian <104913665+maguyber@users.noreply.github.com>
2022-09-26 20:22:05 -0300

    archivo de project_pizzas_front_end

--- insert_products.php
+++ insert_products.php
@@ -5,38 +5,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
+    <title>Document</title>
 </head>
 
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3 class="text-center">INGRESO DE PRODUCTOS</H3>
+                <h3 class="text-center">Ingreso de Productos</h3>
             </div>
             <div class="col-md-12">
-
-                <form class="form-group" accept-charset="utf-8" method="post" action="multi-part">
+                <form class="form-group" accept-charset="utf-8" action="save_products.php" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <br>
                         <label class="control-label" for="producto">PRODUCTO</label>
-                        <input type="text" required="" placeholder="producto" class="form-control" id="producto" 
-                        name="producto">
+                        <input type="text" required=" " placeholder="producto" class="form-control" id="producto" name="producto">
                     </div>
-            </div>
-            <div class="col-md-12">
-
-                <form class="form-group" accept-charset="utf-8" method="post" action="multi-part">
                     <div class="form-group">
                         <br>
-                        <label class="control-label" for="producto">PRECIO</label>
-                        <input type="text" required="" placeholder="producto" class="form-control" id="producto" 
-                        name="producto">
+                        <label class="control-label" for="precio">PRECIO</label>
+                        <input type="text" required=" " placeholder="precio" class="form-control" id="precio" name="precio">
                     </div>
-            </div><div class="form-group"><label class="control-label" for="categoria">CATEGORIA DEL PRODUCTO</label><select id="categoria" name="categoria" class="form-control"><option>hamburguesa</option></select></div>
+                    <div class="form-group">
+                        <br>
+                        <label class="control-label" for="categoria">CATEGORIA DEL PRODUCTO</label>
+                        <select id="categoria" name="categoria" class="form-control">
+                            <?php
+                            include_once("config_products.php");
+                            include_once("db.php");
+                            $link = new Db();
+                            $sql = "select id_category, category_name from categories order by category_name";
+                            $stmt = $link->prepare($sql);
+                            $stmt->execute();
+                            $data = $stmt->fetchAll();
+
+                            foreach ($data as $row) {
+                                echo '<option value="' . $row['id_category'] . '">' . $row['category_name'] . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <br>
+                        <label class="control-label" for="file">seleccione la imagen a subir</label>
+                        <input type="file" id="imagen" class="form-control" name="imagen" size="30">
+                    </div>
+                    <div class="text-center">
+                        <input type="submit" class="btn btn-success" value="añadir producto">
+                    </div>
+                        </form>
+                        </div>
+                        </div>
+                        </div>
+            </div>
+
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
