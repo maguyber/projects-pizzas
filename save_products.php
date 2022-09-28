9295f3470423bc2306688ae51b953972af362c96
Fabian <104913665+maguyber@users.noreply.github.com>
2022-09-28 20:56:53 -0300

    archivos de project_pizzas_front_end

--- save_products.php
+++ save_products.php
@@ -1,5 +1,27 @@
-<?php 
-$product=$_post['producto'];
-$price=$_post['precio'];
-$category=$_post['categoria'];
-echo $product."$price"."$category;]
+<?php
+$product = $_POST['producto'];
+$price = $_POST['precio'];
+$category = $_POST['categoria'];
+
+
+include_once("config_products.php");
+include_once("db.php");
+$link = new Db();
+include_once('upload_image.php');
+$path_img = $directorio . $nombre_img; //ruta completa de la imagen levantada
+$sql = "insert products(product_name,image,price,id_category)values (:product,,:path_img,:price,:category)";
+$stmt = $link->prepare($sql);
+$stmt = $bindvalue('price,$product');
+$stmt = $bindvalue('price,$price');
+$stmt = $bindvalue('category,$category');
+$stmt = $bindvalue('path_img',$path_img);
+if($stmt->execute()){
+    ?>
+    <script>
+    alert("producto Ingresado");
+    windows.location="insert_products.php";
+    </sript>
+    <?php
+
+    
+}
