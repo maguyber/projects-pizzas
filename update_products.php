96c734f1512d38f09ef190d5bc697e57003af172
Fabian <104913665+maguyber@users.noreply.github.com>
2022-10-19 19:11:41 -0300

    Add files via upload

---
+++ update_products.php
@@ -0,0 +1,15 @@
+<?php
+
+$id = $_POST['id'];
+$name = $_POST['nombre'];
+$price = $_POST['precio'];
+$category = $_POST['categoria'];
+
+include_once("config_products.php");
+include_once("db.php");
+$link = new Db();
+$sql="update products set product_name='$name', price=$price, id_category='$category' where id_product=".$id;
+$stmt = $link->prepare($sql);
+$stmt->execute();
+header('location: welcome.php');
+
