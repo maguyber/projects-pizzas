eeee3041aeaf33b368ef9e3f9a30b0f58d5df480
Fabian <104913665+maguyber@users.noreply.github.com>
2022-10-31 20:43:12 -0300

    archivos de project_pizzass_front_end

---
+++ elevadores.php
@@ -0,0 +1,12 @@
+<?php
+class Elevador
+include_once('elevadores.php');
+
+class Montacarga extends Elevador
+{
+private $sizeX;
+private $sizeY;
+
+public function verificarPesoMaximo()
+    private $cantidadMaximoPermitido;
+    private $Estado; 
